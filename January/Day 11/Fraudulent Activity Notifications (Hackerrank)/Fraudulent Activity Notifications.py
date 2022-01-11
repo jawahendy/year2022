5db88ac6aba0d6d0b96084f56f88ff8883624c88
jawahendy <hendyn25@gmail.com>
2022-01-11 09:58:58 +0700

    correction

--- January/Day 11/Fraudulent Activity Notifications (Hackerrank)/Fraudulent Activity Notifications.py
+++ January/Day 11/Fraudulent Activity Notifications (Hackerrank)/Fraudulent Activity Notifications.py
@@ -1,13 +1,63 @@
-def activityNotifications(expenditure, d):
+import math
+import os
+import random
+import re
+import sys
+import bisect as bs
+
+def index(a, x):
+    'Locate the leftmost value exactly equal to x'
+    i = bs.bisect_left(a, x)
+    if i != len(a) and a[i] == x:
+        return i
+    raise ValueError
+
+def median(a_sorted, days):
+    half = len(a_sorted)//2
+    
+    if days % 2:
+        median = a_sorted[half]
+    else:
+        median = (a_sorted[half-1] + a_sorted[half])/2
+        
+    return float(median)
+
+def activityNotifications(log, days):
     # Write your code here
-    total = 0
-    for i in range(d, len(expenditure)):
-        days = expenditure[i-d:i]
-        arr = sorted(days)
-        if d%2 != 0:
-            median = arr[(d+1)//2 - 1]
-        else:
-            median = (arr[(d)//2 - 1] + arr[(d+1)//2])/2
-        if expenditure[i] >= 2 * median:
-            total +=1
-    return total
+
+    heap = sorted(log[:days])
+    res = 0
+    med = 0
+    to_del = 0
+    
+    for ind in range(days, len(log)):
+        med = median(heap, days)
+        #print("heap: {}".format(heap))
+        #print("log[{}] = {} med = {}".format(ind, log[ind], med))
+            
+        if float(log[ind]) >= 2*med:
+            res += 1
+        
+        #del heap[heap.index(log[to_del])]
+        del heap[index(heap, log[to_del])]
+        bs.insort(heap, log[ind])
+        to_del += 1
+            
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    expenditure = list(map(int, input().rstrip().split()))
+
+    result = activityNotifications(expenditure, d)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
