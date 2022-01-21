44087142cd24a75f321f7f79d981604d9379c96c
jawahendy <hendyn25@gmail.com>
2022-01-21 11:33:29 +0700

    never give up disaat pinggang encok

---
+++ January/Day 21/Divisible Sum Pairs(hackerrank)/Divisible sum.py
@@ -0,0 +1,9 @@
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(n-1):
+        j = i+1
+        while j < n:
+            if ((ar[i] + ar[j]) % k) == 0:
+                count += 1
+            j += 1
+    return count
